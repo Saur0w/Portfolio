97bc0075e2fb52063967297dd7a433f79466b684
saur0w <sthapliyal085@gmail.com>
2026-06-14 14:57:51 +0530

    feat: scaffold root layout and initialize landing page component with styles and assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,17 +3,34 @@
 import styles from "./style.module.scss";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
 
-export default function Landing() {
-    return (
-        <section className={styles.landing}>
-            <div className={styles.left}>
+type ImageData = {
+    src: string;
+};
 
+const images: ImageData[] = [
+    { src: "/images/sand.jpg" },
+    { src: "/images/mountain.jpg" },
+    { src: "/images/water.jpg" },
+    { src: "/images/arch.jpg" }
+];
+
+export default function Landing() {
+    const landingRef = useRef<HTMLDivElement>(null);
+    return (
+        <section className={styles.landing} ref={landingRef}>
+            <div className={styles.left}>
+                <div className={styles.heading}>
+                    <h4>INTRODUCTION</h4>
+                    <h1>Saurow</h1>
+                    <p>I design and develop immersive web experiences that feel intuitive, memorable, and alive.</p>
+                </div>
             </div>
             <div className={styles.right}>
-                
+
             </div>
         </section>
     );
