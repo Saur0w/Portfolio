4fb9d4d4694bad46fdaab7c1e2d4c05a39137b25
saur0w <sthapliyal085@gmail.com>
2026-06-17 22:48:43 +0530

    feat: add Des component with initial styling and layout structure

--- src/components/Des/index.tsx
+++ src/components/Des/index.tsx
@@ -5,7 +5,14 @@ import styles from "./style.module.scss";
 export default function Des() {
     return (
         <section className={styles.des}>
-            
+            <div className={styles.left}>
+                <div className={styles.text}>
+                    <p>I build identities that move, sometimes polished, sometimes raw, always intentional.</p>
+                </div>
+            </div>
+            <div className={styles.right}>
+                
+            </div>
         </section>
     )
 }
